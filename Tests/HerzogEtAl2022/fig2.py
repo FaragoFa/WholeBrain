32dea48c767b5cfb67a6e646422edda7edf59d16
Facundo <facundofarago@gmail.com>
2023-10-20 20:02:16 -0300

    cambios y resultados

--- Tests/HerzogEtAl2022/fig2.py
+++ Tests/HerzogEtAl2022/fig2.py
@@ -36,8 +36,8 @@ integrator.verbose = False
 # ============== chose a FIC mechanism
 import WholeBrain.Utils.FIC.BalanceFIC as BalanceFIC
 BalanceFIC.integrator = integrator
-import WholeBrain.FIC.Balance_Herzog2022 as Herzog2022Mechanism
-import WholeBrain.FIC.Balance_DecoEtAl2014 as Deco2014Mechanism
+import WholeBrain.Utils.FIC.Balance_Herzog2022 as Herzog2022Mechanism
+import WholeBrain.Utils.FIC.Balance_DecoEtAl2014 as Deco2014Mechanism
 BalanceFIC.balancingMechanism = Herzog2022Mechanism  # default behaviour for this project
 
 
